4bb6bee4f0246723592987d98a13d8f715eca4f1
Allison McMillan <allison.p.mcmillan@gmail.com>
2018-01-11 17:25:47 -0500

    additional tests that cover more enumerable methods to explore

--- exercises/additional_tests.rb
+++ exercises/additional_tests.rb
@@ -1,76 +1,163 @@
-#### 3 ways to write a block - do, {}, and (&:xxx)
-def triple(val)
-  val * 3
-end
+gem 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
 
-def triple_values_do
-  values = [1, 2, 3]
-  # transform values with a do block
-  assert_equal values, [3, 6, 9]
-end
+class AdditionalTests < Minitest::Test
+  #### 3 ways to write a block - do, {}, and (&:xxx)
+  class NumberTripler
+    def initialize(val) #initialize the class with a number
+      @val = val #which is then stored inside the class
+    end
 
-def triple_values_braces
-  values = [1, 2, 3]
-  # transform values with a { } block
-  assert_equal values, [3, 6, 9]
-end
+    def triple
+      @val * 3 #when you call the triple method, it takes the value stored, multiplies it by 3 and stores that
+    end
 
-def triple_values_method
-  values = [1, 2, 3]
-  # transform values with a method reference
-  assert_equal values, [3, 6, 9]
-end
+    def log(&block)
+      #puts around block of code, logs out result of the block
+      puts "let's start this block"
+      puts block.call
+      puts "this block is so last year"
+    end
+  end
+
+  def test_triple_values_do
+    values = [NumberTripler.new(1), NumberTripler.new(2), NumberTripler.new(3)]
+    values = values.map do |value|
+      #value is the object NumberTripler.new(1) and so then we need to call triple on that value. Value is an object and no longer just a number
+      value.triple
+    end
+    assert_equal [3, 6, 9], values
+  end
+
+  def test_triple_values_braces
+    values = [NumberTripler.new(1), NumberTripler.new(2), NumberTripler.new(3)]
+    values = values.map { |value| value.triple }
+    assert_equal [3, 6, 9], values
+  end
+
+  def test_triple_values_method
+    values = [NumberTripler.new(1), NumberTripler.new(2), NumberTripler.new(3)]
+    values = values.map(&:triple)
+    assert_equal [3, 6, 9], values
+  end
+
+  def test_logging
+    value = NumberTripler.new(1)
+    value.log do
+      1
+    end
+  end
 
 
-#### .with_index
-def values_with_index
-  names = ["Alice", "Bob", "Cecil"]
+  #### .with_index
+  def test_values_with_index
+    names = ["Alice", "Bob", "Cecil"]
+    names = names.map.with_index do |name, index|
+      "#{index}. #{name}"
+    end
 
-  # write a block that adds the index to the beginning of each name
+    assert_equal ["0. Alice", "1. Bob", "2. Cecil"], names
+  end
 
-  assert_equal names, ["0. Alice", "1. Bob", "2. Cecil"]
-end
+  #### .index_by? Is that what Steve mentioned?
+  def test_values_index_by
+    #this should be an example of
+    users = [
+      {
+        id: 1,
+        name: "Alice Smith",
+        active: true
+      },
+      {
+        id: 2,
+        name: "Bob",
+        active: true
+      },
+      {
+        id: 3,
+        name: "Camilla Johnson",
+        active: false
+      },
+      {
+        id: 4,
+        name: "Doug Brown",
+        active: true
+      },
+      {
+        id: 5,
+        name: "Emil",
+        active: false
+      }
+    ]
+    users.index_by do |user|
+      user[:id]
+    end
 
-#### .by_index? Is that what Steve mentioned?
+    assert_equal({1 => {
+        id: 1,
+        name: "Alice Smith",
+        active: true
+      }, 2 => {
+        id: 2,
+        name: "Bob",
+        active: true
+      }, 3 => {
+        id: 3,
+        name: "Camilla Johnson",
+        active: false
+      }, 4 => {
+        id: 4,
+        name: "Doug Brown",
+        active: true
+      },5 => {
+        id: 5,
+        name: "Emil",
+        active: false
+      }
+    }, users)
+  end
 
 
-#### &, |, +, -
-def intersection
-  evens = [2, 4, 6, 8, 10, 12]
-  every_third = [3, 6, 9, 12]
+  #### &, |, +, -
+  def test_intersection
+    evens = [2, 4, 6, 8, 10, 12]
+    every_third = [3, 6, 9, 12]
 
-  # write code that gets the intersection of even numbers and every_third number
+    intersection = evens & every_third
 
-  assert_equal intersection, [6, 12]
-end
+    assert_equal [6, 12], intersection
+  end
 
-def union
-  current_users = ["Alice", "Bob", "Candice", "Emily"]
-  new_users = ["Bob", "Doug", "Emily"]
+  def test_union
+    current_users = ["Alice", "Bob", "Candice", "Emily"]
+    new_users = ["Bob", "Doug", "Emily"]
 
-  # write code that combines the two array without duplicates
+    union = (current_users | new_users).sort
+    #need to sort the union so that the array is in the same order and the duplicate is removed correctly
 
-  assert_equal union, ["Alice", "Bob", "Candice", "Doug", "Emily"]
-end
+    assert_equal ["Alice", "Bob", "Candice", "Doug", "Emily"], union
+  end
 
-def add
-  current_users = ["Alice", "Bob", "Candice", "Emily"]
-  new_users = ["Bob", "Doug", "Emily"]
+  def test_add
+    current_users = ["Alice", "Bob", "Candice", "Emily"]
+    new_users = ["Bob", "Doug", "Emily"]
 
-  # write code that combines the arrays with duplicates
+    appended = current_users + new_users
 
-  assert_equal appended, ["Alice", "Bob", "Candice", "Emily", "Bob", "Doug", "Emily"]
-end
+    assert_equal ["Alice", "Bob", "Candice", "Emily", "Bob", "Doug", "Emily"], appended
+  end
 
-def subtract
-  all_user_ids = [1, 2, 3, 4, 5]
-  inactive_user_ids = [1, 4, 5, 6]
+  def test_subtract
+    all_user_ids = [1, 2, 3, 4, 5]
+    inactive_user_ids = [1, 4, 5, 6]
 
-  # write code that gets the active user ids
+    active_user_ids = all_user_ids - inactive_user_ids
 
-  assert_equal active_user_ids, [2, 3]
+    assert_equal [2, 3], active_user_ids
 
-  # write code that gets users that shouldn't be in the system :)
+    bad_user_ids = inactive_user_ids - all_user_ids
 
-  assert_equal bad_user_ids, [6]
+    assert_equal [6], bad_user_ids
+  end
 end
